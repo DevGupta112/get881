d2cdab2d187acf959665ce90832a1f1997025f29
DevGupta112 <devashish.gupta1998@gmail.com>
2020-12-17 17:00:02 +0530

    Added Sample Class

---
+++ DemoApp/src/com/lti/model/Sample.java
@@ -0,0 +1,12 @@
+package com.lti.model;
+
+public class Sample {
+	public void show() {
+		System.out.println("HelloWorld");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
