e5d7e339738fe3b4327c45dfcc6172e6bab0e82c
DevGupta112 <54732775+DevGupta112@users.noreply.github.com>
2020-12-18 15:44:39 +0530

    dummy method added

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,5 +4,8 @@ public class Calc {
 	public void add(int num1,int num2) {
 		System.out.println("Sum= "+(num1+num2));
 	}
+	public void add2(int num1,int num2) {
+		System.out.println("Difference naya dala= "+(num1-num2));
+	}
 	
 }
